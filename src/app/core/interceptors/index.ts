eb17f1ac03852ae4dc694761880f67261870d241
eslamsalah5 <eslamsalah5346@gmail.com>
2025-05-11 05:36:25 +0300

    add localization to company profile and make his design more better

--- src/app/core/interceptors/index.ts
+++ src/app/core/interceptors/index.ts
@@ -1,8 +1,6 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './auth.interceptor';
-import { ApiInterceptor } from './api.interceptor';
 
 export const httpInterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-  { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true },
 ];
